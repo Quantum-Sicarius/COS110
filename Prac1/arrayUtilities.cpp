5fd91784de5150b2fd3b40bf9083b472185063cc
Thomas <thomas@quantum-sicarius.za.net>
2016-08-11 22:34:30 +0200

    Somewhat working code.

--- Prac1/arrayUtilities.cpp
+++ Prac1/arrayUtilities.cpp
@@ -1,10 +1,225 @@
+#include <stdlib.h>
+#include <sstream>
 #include "arrayUtilities.h"
 using namespace std;
+
 
 // Implement functions defined in arrayUtilities.h in this file.
 // Note: arrayUtilities.h will be overwritten by Fitchfork.
 
+int numDaysInMonth(int year, int month) {
+        switch (month) {
+        case 1:
+                return 31;
+                break;
+        case 2:
+                // Check leap year
+                if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+                        return 29;
+                        break;
+                }
+                return 28;
+                break;
+        case 3:
+                return 31;
+                break;
+        case 4:
+                return 30;
+                break;
+        case 5:
+                return 31;
+                break;
+        case 6:
+                return 30;
+                break;
+        case 7:
+                return 31;
+                break;
+        case 8:
+                return 31;
+                break;
+        case 9:
+                return 30;
+                break;
+        case 10:
+                return 31;
+                break;
+        case 11:
+                return 30;
+                break;
+        case 12:
+                return 31;
+                break;
+        }
+
+}
+
+size_t* yearsMonthSize(int year) {
+        size_t *yearmonthsize = new size_t[12];
+
+        for (size_t i = 0; i < 12; i++) {
+                yearmonthsize[i] = numDaysInMonth(year, i + 1);
+        }
+
+        return yearmonthsize;
+}
+/*
+   void parseDateStamp(std::string stampString, int *stampArray[3]) {
+
+        string delimiter = "/";
+
+        int *year = new int;
+   *year = stoi(stampString.substr(0, stampString.find(delimiter)));
+        stampString.erase(0, stampString.find(delimiter) + delimiter.length());
+
+        int *month = new int;
+   *month = stoi(stampString.substr(0, stampString.find(delimiter)));
+        stampString.erase(0, stampString.find(delimiter) + delimiter.length());
+
+        stampString = (stampString.substr(0, stampString.find(delimiter)));
+
+        int *day = new int;
+   *day = stoi(stampString.substr(0, stampString.find("-")));
+
+   *stampArray[0] = *year;
+   *stampArray[1] = *month;
+   *stampArray[2] = *day;
 
 
+        cout << "SUCCESS" << endl;
+        cout << *stampArray[0] << *stampArray[1] << *stampArray[2];
+   }
+   //*/
+void parseDateStamp(std::string stampString, int *stampArray[3]){
+        std::string word = "";
+        std::istringstream buffer;
+
+        size_t i;
+        for(i = 0; stampString.at(i) != '/'; i++)
+                word.push_back(stampString.at(i));
 
 
+        i++;
+        buffer.str(word);
+        buffer >> (*stampArray)[0];
+
+
+        buffer.clear();
+        word.clear();
+
+        for(i; stampString.at(i) != '/'; i++) {
+                word.push_back(stampString.at(i));
+        }
+
+
+        i++;
+
+        buffer.str(word);
+
+        buffer >> (*stampArray)[1];
+        buffer.clear();
+
+        word.clear();
+
+        for(i; stampString.length() > i; i++)
+                word.push_back(stampString.at(i));
+
+        i++;
+
+        buffer.str(word);
+        buffer >> (*stampArray)[2];
+        buffer.clear();
+        word.clear();
+
+}
+
+size_t parseDataFile(std::string const & filename, short ***&dataPtr, int &firstYear) {
+
+        fstream file;
+        file.open(filename);
+
+        string date;
+        string mill;
+
+        int count = 0;
+
+        // First date.
+        file >> date >> mill;
+
+        count++;
+        reallocShort3d(dataPtr, 0, count);
+
+        //cout << date << "\t" << mill << endl;
+
+        int *parseArr = new int[3];
+        parseDateStamp(date,&parseArr);
+
+        firstYear = parseArr[0];
+
+        // New 2d array
+        size_t *days = yearsMonthSize(parseArr[0]-firstYear);
+        dataPtr[parseArr[0]-firstYear] = newShort2d(12,days);
+
+        dataPtr[parseArr[0]-firstYear][parseArr[1]-1][parseArr[2]-1] = stoi(mill);
+
+        // Previous year
+        int Previous_year = parseArr[0];
+        // Year count
+        int year_count = 1;
+
+        while (file >> date >> mill) {
+                // First date.
+                file >> date >> mill;
+
+                count++;
+
+                // Read array
+                int *parseArr = new int[3];
+                parseDateStamp(date,&parseArr);
+
+                if (Previous_year != parseArr[0]) {
+                        Previous_year = parseArr[0];
+                        year_count++;
+                        reallocShort3d(dataPtr, year_count - 1, year_count);
+
+                        //cout << "NEW MONTH!" << endl;
+                        // New 2d array
+                        size_t *days = yearsMonthSize(parseArr[0]-firstYear);
+                        dataPtr[parseArr[0]-firstYear] = newShort2d(12,days);
+                }
+
+                dataPtr[parseArr[0]-firstYear][parseArr[1]-1][parseArr[2]-1] = stoi(mill);
+                //cout << parseArr[0] << "\t" << parseArr[1] << "\t" << parseArr[2] << "\t" << mill << endl;
+                //cout << dataPtr[parseArr[0]-firstYear][parseArr[1]-1][parseArr[2]-1] << endl;
+
+
+        }
+}
+
+void reallocShort3d(short ***&ptr, size_t oldSize, size_t newSize) {
+        //cout << "REALLOCATION 3D" << endl;
+        //cout << "OLDSIZE: " << oldSize << endl;
+        //cout << "NEWSIZE: " << newSize << endl;
+
+        short ***newptr = new short **[newSize];
+
+        for(size_t i = 0; i < oldSize; i++) {
+                newptr[i] = ptr[i];
+        }
+
+        delete [] ptr;
+        ptr = newptr;
+}
+short ** newShort2d(size_t numRow, size_t const *colSize) {
+        //cout << "REALLOCATION" << endl;
+        //cout << "numRow: " << numRow << endl;
+        //cout << "colSize: " << colSize << endl;
+
+        short **newshort = new short *[numRow];
+        for (size_t i = 0; i < *colSize; i++) {
+                newshort[i] = new short[*colSize];
+        }
+        return newshort;
+}
+void deleteShort2d(short **&ptr, size_t rowSize) {
+}
--- Prac1/arrayUtilities.h
+++ Prac1/arrayUtilities.h
@@ -1,5 +1,11 @@
 #ifndef RAIN_GRAPH_UTILITIES_H
 #define RAIN_GRAPH_UTILITIES_H
+#include <iostream>   // std::cout
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <stdlib.h>
+#include<cstddef>
 
 
 /**
@@ -11,8 +17,8 @@
 *	else set false
 *
 * @param year represents the year an integer value.
-* @param month represents the month as an integer value, 
-*     where a value of zero indicates January, 
+* @param month represents the month as an integer value,
+*     where a value of zero indicates January,
 *     a value of one February, and so forth.
 *
 * @return the calculated number of days
@@ -23,12 +29,12 @@ int numDaysInMonth(int year, int month);
 /**
 * @brief	Calculate all the month's total number of days for the year.
 *
-* int numDaysInMonth(int year, int month) 
+* int numDaysInMonth(int year, int month)
 * may be used to calculate this information.
 *
-* @return an one dimensional array with 12 cells - one for each month. 
-* Each cell should contain the amount of days of a specific month. 
-* The first cell represents January's number of days, 
+* @return an one dimensional array with 12 cells - one for each month.
+* Each cell should co ntain the amount of days of a specific month.
+* The first cell represents January's number of days,
 * the second cell repersents Februry'snumber of dats, etc.
 */
 size_t* yearsMonthSize(int year);
@@ -38,7 +44,7 @@ size_t* yearsMonthSize(int year);
 * @brief	convert the string in the form YYYY/MM/DD, to integer values.
 *
 * @param	stampString is the string to be converted.
-* @param	stampArray contains the converted information, 
+* @param	stampArray contains the converted information,
 * 			index 0 represents the year, 1 the month, and 2 the day.
 *
 * NOTE: stampArray must store -1 at the indexes where the conversion failed.
@@ -49,36 +55,36 @@ void parseDateStamp(std::string stampString, int *stampArray[3]);
 /**
 * @brief	Parse a file containing the rainfall data into a 3D array
 *
-* @details This function must parse the opened file 
+* @details This function must parse the opened file
 * and read the values to dataPtr.
 *
-*	The data in the file is represented by a timestamp paired with a value. 
+*	The data in the file is represented by a timestamp paired with a value.
 *	The value indicates the amount of rain for that day in millimetres.
-*	The value is separated with a tab. 
-*	Each timestamp value pair is on a new line. 
+*	The value is separated with a tab.
+*	Each timestamp value pair is on a new line.
 *
 *	It is assumed that all timestamps will follow consecutively,
-*	however, the apparatus taking these values may sometimes fail. 
-*	The absence of an entry for a day indicates that the apparatus 
-*	has failed on that specific day.  The value of -1 must be stored 
+*	however, the apparatus taking these values may sometimes fail.
+*	The absence of an entry for a day indicates that the apparatus
+*	has failed on that specific day.  The value of -1 must be stored
 *	in dataPtr to indicate the absence of that data.
-*	
+*
 *	dataPtr must be able to grow dynamically, with the help of the relevant
-*	array functions. dataPtr may only grow with one extra year element 
-*	at a time. This growth must be triggered if the function detects a new 
+*	array functions. dataPtr may only grow with one extra year element
+*	at a time. This growth must be triggered if the function detects a new
 *	year in a timestamp.
 *
-*	The month-dimension's size will always be 12. The day-dimension's 
+*	The month-dimension's size will always be 12. The day-dimension's
 *	size must be adapted according to the number of days in the month.
-*		
+*
 *	The use of the following functions are recommended:
-*		void reallocShort3d(short ***&ptr, size_t oldSize, size_t newSize) 
-*		short ** newShort2d(size_t numRow, size_t *colSize) 
+*		void reallocShort3d(short ***&ptr, size_t oldSize, size_t newSize)
+*		short ** newShort2d(size_t numRow, size_t *colSize)
 *		size_t* yearsMonthSize(int year)
 *
 * @param filename is the file to be opened.
 *
-* @param dataPtr is the data-structure that will contain all read in values. 
+* @param dataPtr is the data-structure that will contain all read in values.
 * Note that dataPtr is a referance to a 3D array.
 * The dimensions can be described as [year][month][day].
 *
@@ -90,43 +96,43 @@ size_t parseDataFile(std::string const & filename, short ***&dataPtr, int &first
 
 
 /**
-* @brief reallocates/resizes a 3D array of shorts to fit into a new size. 
+* @brief reallocates/resizes a 3D array of shorts to fit into a new size.
 * Size here refers to the first dimension. i.e. ptr[size][row][col].
 *
-* @note after the function executed, the original data in the array must 
-* not be altered in any way. That is to say, only the first dimension of 
+* @note after the function executed, the original data in the array must
+* not be altered in any way. That is to say, only the first dimension of
 * ptr may be altered.
 *
-* @NOTE! this function must work in general and not specifically 
+* @NOTE! this function must work in general and not specifically
 *	in the way it is used in parseDataFile
 *
 * @param ptr is the pointer that refers to the 3D array to be resized.
-* @param oldSize is the current total number of elements in the first 
+* @param oldSize is the current total number of elements in the first
 *		dimension of ptr. e.g.   ptr[this amount here][row][col]
-* @param newSize is the new size that ptr will have after the function 
+* @param newSize is the new size that ptr will have after the function
 * has been executed.   ptr[the new amount here][row][col]
 */
 void reallocShort3d(short ***&ptr, size_t oldSize, size_t newSize);
 
 
 /**
-* @brief creates a new varied length 2D array of shorts. 
+* @brief creates a new varied length 2D array of shorts.
 * To clarify, the amount of columns in each row may vary
-*		e.g. row one might look like this {1, 3, 5, 6, END}, with a total 
-*		of four columns, where row two might look like this {1, 5, END} 
+*		e.g. row one might look like this {1, 3, 5, 6, END}, with a total
+*		of four columns, where row two might look like this {1, 5, END}
 *		with a total of 2 columns, and row three, {4, 5, 7, END}
 *				|__COL__
 *				R[1][3][5][6]
 *				O[1][5]
 *				W[4][5][7]
 *|
-* @NOTE! this function must work in general and not specifically 
+* @NOTE! this function must work in general and not specifically
 *	in the way it is used in parseDataFile
 
-* @param numRow is the number of rows that the created array will have. 
+* @param numRow is the number of rows that the created array will have.
 *	In the example above, it is 3
-* @param colSize is a one dimensional array that indicates the number 
-*	of columns for each of the rows. In the example above, 
+* @param colSize is a one dimensional array that indicates the number
+*	of columns for each of the rows. In the example above,
 *	the array would look like this: {4, 2, 3}
 *
 * @return will return the 2D pointer that was created.
@@ -137,24 +143,12 @@ short ** newShort2d(size_t numRow, size_t const *colSize);
 /**
 * @brief This function will delete all the elements af a given 2D array.
 *
-* @param ptr is the pointer, pointing to the data that is to be deleted. 
+* @param ptr is the pointer, pointing to the data that is to be deleted.
 *       this pointer must be set to zero after the deletion process
-* @param rowSize is the total rows in the array, i.g. ptr[row][col]. 
+* @param rowSize is the total rows in the array, i.g. ptr[row][col].
 *       The function must loop through every row to delete its columns.
 */
 void deleteShort2d(short **&ptr, size_t rowSize);
 
 
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
