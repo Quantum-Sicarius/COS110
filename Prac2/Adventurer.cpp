6eabf54b6064a395ee60b907b0ee40ccc2e8a904
Thomas <thomas@quantum-sicarius.za.net>
2016-08-21 16:07:23 +0200

    Basic implementation.

--- Prac2/Adventurer.cpp
+++ Prac2/Adventurer.cpp
@@ -1,2 +1,125 @@
-
+#include "Adventurer.h"
+#include <iostream>
 //Implement or define your class here
+Adventurer::Adventurer() {
+        //this->items;
+        this->currentNumberOfItems = 0;
+        this->currentCarryWeight = 0.0;
+} //default constructor
+
+Adventurer::Adventurer(const Adventurer &a) {
+        Adventurer* copy = new Adventurer;
+        copy->setName(a.getName());
+        copy->setHealth(a.getHealth());
+        copy->setMaxCarryWeight(a.getMaxCarryWeight());
+        copy->setMaxNumberOfItems(a.getMaxNumberOfItems());
+        copy->setCurrentCarryWeight(a.getCurrentCarryWeight());
+        copy->setCurrentNumberOfItems(a.getCurrentNumberOfItems());
+}  //copy constructor
+Adventurer::~Adventurer() {
+        //delete &health;
+        //delete &maxCarryWeight;
+        //delete &maxNumberOfItems;
+        //delete &currentCarryWeight;
+        //delete &currentNumberOfItems;
+        //delete &name;
+}
+
+void resizeArr(int oldSize, int newSize, string*** &arr) {
+
+        cout << "Redo Array" << endl;
+
+        string ***newArr = new string**[newSize];
+
+        cout << "LOOP" << endl;
+        cout << oldSize << endl;
+        for (int i = 0; i < oldSize; i++) {
+                newArr[i] = arr[i];
+        }
+        cout << "DEL" << endl;
+        delete [] arr;
+        cout << "REASSIGN" << endl;
+        arr = newArr;
+}
+
+
+bool Adventurer::pickUpItem(string it, double weight) {
+        // Check if the player can carry more items.
+        if ((currentCarryWeight + weight) > maxCarryWeight || (currentNumberOfItems + 1) > (maxNumberOfItems)) {
+                return false;
+        } else {
+                // Resize the Array.
+
+                resizeArr(this->getCurrentNumberOfItems(), this->getCurrentNumberOfItems() + 1, this->items);
+                this->setCurrentNumberOfItems(this->getCurrentNumberOfItems() + 1);
+
+                cout << "SET ITEMS" << endl;
+                cout << this->getCurrentNumberOfItems() << endl;
+                cout << this->items[0] << endl;
+
+                *this->items[this->getCurrentNumberOfItems() - 1] = new string[2];
+                cout << *this->items[this->getCurrentNumberOfItems() - 1];
+
+                *this->items[this->getCurrentNumberOfItems() - 1][0] = it;
+                *this->items[this->getCurrentNumberOfItems() - 1][1] = to_string(weight);
+
+
+
+                return true;
+        }
+}
+bool Adventurer::dropItem(string it) {
+        // TODO
+        return true;
+}
+bool Adventurer::dropItem(int index) {
+        // TODO
+        return true;
+}
+void Adventurer::setName(string n) {
+        this->name = n;
+}
+string Adventurer::getName() const {
+        return this->name;
+}
+void Adventurer::setMaxCarryWeight(double w) {
+        this->maxCarryWeight = w;
+}
+double Adventurer::getMaxCarryWeight() const {
+        return this->maxCarryWeight;
+}
+void Adventurer::setCurrentCarryWeight(double w) {
+        this->currentCarryWeight = w;
+}
+double Adventurer::getCurrentCarryWeight() const {
+        return this->currentCarryWeight;
+}
+void Adventurer::setMaxNumberOfItems(int n) {
+        this->maxNumberOfItems = n;
+}
+int Adventurer::getMaxNumberOfItems() const {
+        return this->maxNumberOfItems;
+}
+void Adventurer::setCurrentNumberOfItems(int n) {
+        this->currentNumberOfItems = n;
+}
+int Adventurer::getCurrentNumberOfItems() const {
+        return this->currentNumberOfItems;
+}
+int Adventurer::getNumberOfAdventurers() const {
+        // TODO
+        return 0;
+}
+void Adventurer::setHealth(double h) {
+        this->health = h;
+}
+double Adventurer::getHealth() const {
+        return this->health;
+}
+string** Adventurer::getItem(int index) const {
+        return this->items[index];
+}
+
+// Copy.
+Adventurer& Adventurer::operator = (const Adventurer& a) {
+}
--- Prac2/main.cpp
+++ Prac2/main.cpp
@@ -1,10 +1,118 @@
+#include <iostream>
+#include "Adventurer.h"
+
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
+#include <string>
 /*
  * Add comments here
  */
 
-int main() {
-    //add code here to test your class
-    
-    return 0;
+using namespace std;
+
+int adventurers = 0;
+
+string items[][2] = {
+{"SWORD", "21.2"},
+{"AXE", "30.1"},
+{"MACE","12.1"},
+{"BROADSWORD", "15"},
+{"DAGGER", "5.1"}
+};
+
+string* getRandomItem() {
+        int random_num;
+        // Generate random number between 1 and 5.
+        random_num = rand() % 5;
+
+        //cout << ran
+
+        //cout << "RANDOM: " << random_num << endl;
+        //cout << items[0][0];
+        return items[random_num];
 }
 
+int randomNumber() {
+
+        // Random.
+        // Initialize random seed.
+
+        int random_num;
+        // Generate random number between 1 and 100.
+        random_num = rand() % 100 + 1;
+
+        //cout << "RANDOM: " << random_num << endl;
+        return random_num;
+}
+
+void testAttributes(Adventurer *ad) {
+
+        adventurers++;
+        //cout << adventurers;
+
+        // Set name.
+        ad->setName (("adventurer" + to_string(adventurers)));
+        cout << "Adventurer name: " << ad->getName() << endl;
+
+        // Set carry weight.
+        ad->setMaxCarryWeight(randomNumber());
+        cout << "Adventurer: " << ad->getName() << "\tmax carry weight: " << ad->getMaxCarryWeight() << endl;
+
+        // Set max items;
+        ad->setMaxNumberOfItems(randomNumber());
+        cout << "Adventurer: " << ad->getName() << "\tmax number of items: " << ad->getMaxNumberOfItems() << endl;
+
+        // Set health;
+        ad->setHealth(randomNumber());
+        cout << "Adventurer: " << ad->getName() << "\thealth: " << ad->getHealth() << endl;
+
+        // Pick up item.
+        string *item = getRandomItem();
+
+        cout << item[0] << "\t" << item[1] << endl;
+
+        cout << "Adventurer: " << ad->getName() << " is picking up: " << item[0] << " of weight: " << item[1]
+             << "\tSuccess? " << ad->pickUpItem(item[0],stod(item[1])) << endl;
+
+        // Get item.
+        //cout << "Adventurer: " << ad->getName() << " has item: " << ad->getItem(0)[0] << " in index 0" << end;
+}
+
+void printAttributes(Adventurer *ad) {
+
+        cout << "Adventurer name: " << ad->getName() << endl;
+
+        cout << "Adventurer: " << ad->getName() << "\tmax carry weight: " << ad->getMaxCarryWeight() << endl;
+
+        cout << "Adventurer: " << ad->getName() << "\tmax number of items: " << ad->getMaxNumberOfItems() << endl;
+
+        cout << "Adventurer: " << ad->getName() << "\thealth: " << ad->getHealth() << endl;
+}
+
+int main() {
+        //add code here to test your class
+        cout << "Start..." << endl;
+
+        srand (time(NULL));
+
+        //cout << getRandomItem() << endl;
+
+        // Create new Adventurer.
+        Adventurer *ad1 = new Adventurer();
+        testAttributes(ad1);
+
+        Adventurer *ad2 = new Adventurer();
+        testAttributes(ad2);
+
+        Adventurer *ad3 = ad2;
+        printAttributes(ad3);
+
+        cout << "Deleting AD2" << endl;
+        delete ad2;
+
+        cout << "Testing AD3" << endl;
+        printAttributes(ad3);
+
+        return 0;
+}
