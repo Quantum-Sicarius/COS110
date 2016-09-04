72c74c7ca471c166b2013e8709cb8d48e4c8272b
Thomas <thomas@quantum-sicarius.za.net>
2016-09-04 22:45:34 +0200

    meh

--- Prac3/library.h
+++ Prac3/library.h
@@ -1,3 +1,6 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
 #include <string>
 #include "book.h"
 #include <iostream>
@@ -5,10 +8,18 @@
 using namespace std;
 
 class Library {
+  friend class Librarian;
+
+private:
   string name;
   Book** books;
   int librarySize;
   int numBooks;
+  Library operator+=(Book* b);
+  Library operator-=(Book* b);
+  Library& operator=(const Library &l);
+  Library& operator++(int);
+  Library& operator--();
 
 public:
   Library(){
@@ -17,17 +28,12 @@ public:
     numBooks = 0;
     books = new Book*[5];
   }
+  Library(Library& lib, const string &name);
   Library(string n): name(n), books(0), librarySize(5), numBooks(0) {
     books = new Book*[5];
   }
   Library(const Library&);
   ~Library();
-
-  Library operator+=(Book* b);
-  Library operator-=(Book* b);
-  Library& operator=(const Library &l);
-  Library& operator++(int);
-  Library& operator--();
 
   Book* getBook(string);
   bool isFull();
@@ -35,3 +41,5 @@ public:
   string getName();
   void setName(string s);
 };
+
+#endif
