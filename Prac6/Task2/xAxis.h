fc4e6628283ccc29d9bba9e0c18ec0944aaf2389
Thomas <thomas@quantum-sicarius.za.net>
2016-09-21 17:49:49 +0200

    Moved structure.

--- Prac6/Task2/xAxis.h
+++ Prac6/Task2/xAxis.h
@@ -3,10 +3,10 @@
 #ifndef XAXIS_H
 #define XAXIS_H
 
-#include <i0stream>
+#include <iostream>
 #include <string>
 
-using namespace sfd;
+using namespace std;
 
 class xAxis{
 
@@ -20,9 +20,6 @@ private:
 	node *head;
 
 public:
-
-
-
 	~xAxis();
 	xAxis();
 	/* constructor: length is the length of the x-axis
@@ -32,17 +29,17 @@ If the value is negative then store it as 0.	*/
 	xAxis(int length, string* n, double* v);
 
 	/*appends one node to the linked list with the value v and the label n
-If the value is negative then store it as 0.*/																																									
+If the value is negative then store it as 0.*/
 	void extend(double v,string n);
 
 	/*removes the last node from the linked list*/
 	void shorten();
 	/*removes the node with the label n from the linked list*/
 	void deleteNode(string n);
-	/*increases the value of the node with label n by val*/																																																																																	.
+	/*increases the value of the node with label n by val*/
 	void increaseValueBy(string n,double val);
 	/*decreases the value of the node with label n by val
-If the value becomes negative then store it as 0.*/
+	If the value becomes negative then store it as 0.*/
 	void decreaseValueBy(string n,double val);
 	/*the node which has the label o must be changed so that it has the label n*/
 	void changeLabel(string o,string n);
